7fb56e6e346ba32462846f29f31fb03e2fac7180
Shay Howard-Bennett <shamus360@googlemail.com>
2022-11-09 15:21:35 +0000

    window open

--- main.cpp
+++ main.cpp
@@ -22,8 +22,7 @@
 #include <glad.c>
 #include <GLFW/glfw3.h> 
 
-#define HEIGHT 1000
-#define WIDTH 1000
+
 
 int main(int argc, char*argv[]){
   
@@ -39,18 +38,24 @@ int main(int argc, char*argv[]){
       
  //create window
   GLFWwindow* window;
+  int height = 1080,
+      width = 1920;
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+   GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+   const GLFWvidmode* videomode = glfwGetVideoMode(monitor);
+
   if( argv[1]!= NULL && strcmp(argv[1], "fullscreen") == 0){
-    GLFWmonitor* monitor = glfwGetPrimaryMonitor();
-    window = glfwCreateWindow(HEIGHT, WIDTH, "Fullscreen window", monitor, NULL);
+    height = videomode->height;
+    width = videomode->width;
+    window = glfwCreateWindow(height, width, "Fullscreen window", monitor, NULL);
    }
    else{
-    window = glfwCreateWindow(1000, 1000, "Windowed window", NULL, NULL);
-
+    window = glfwCreateWindow(height, width, "Windowed window", NULL, NULL);
    }
+
    
   //make window context current
    glfwMakeContextCurrent(window);
@@ -60,9 +65,8 @@ int main(int argc, char*argv[]){
   //contextualisation
    gladLoadGL();
   
-
-  //set the viewport
-   glViewport(0,0,HEIGHT,WIDTH);
+  //set the viewport            todo:: viewport does not draw whole screen in fullscreen mode
+   glViewport(0,0,height,width);
   //set clear color
    glClearColor(0.5f,0.5f,0.1f,1.0f);
   
