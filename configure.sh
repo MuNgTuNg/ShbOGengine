c9ced50990b60a0780eb8f8f3af97c8589fc3674
Shay Howard-Bennett <shamus360@googlemail.com>
2022-11-09 14:52:29 +0000

    window open

--- configure.sh
+++ configure.sh
@@ -1,3 +1,3 @@
-#! /bin/bash/
+#! /bin/bash
 
 cmake -S . -B build
--- main.cpp
+++ main.cpp
@@ -20,40 +20,51 @@
 
 //external
 #include <glad.c>
-
 #include <GLFW/glfw3.h> 
+
+#define HEIGHT 1000
+#define WIDTH 1000
 
 int main(int argc, char*argv[]){
   
-
-
- 
+ //initialise glfw
   if (!glfwInit())
   {
     fprintf(stderr, "Failed to initialize GLFW\n");
     exit(EXIT_FAILURE);
   }
   
-      
 
+  
+      
+ //create window
   GLFWwindow* window;
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   if( argv[1]!= NULL && strcmp(argv[1], "fullscreen") == 0){
     GLFWmonitor* monitor = glfwGetPrimaryMonitor();
-    window = glfwCreateWindow(1000, 1000, "Fullscreen window", monitor, NULL);
+    window = glfwCreateWindow(HEIGHT, WIDTH, "Fullscreen window", monitor, NULL);
    }
    else{
     window = glfwCreateWindow(1000, 1000, "Windowed window", NULL, NULL);
 
    }
-   glfwMakeContextCurrent(window);
-
    
+  //make window context current
+   glfwMakeContextCurrent(window);
+ 
+
+  //glad is loaded on current context so it must be after window creation and 
+  //contextualisation
    gladLoadGL();
+  
 
-   glViewport(0,0,1000,1000);
-
-   glClearColor(255,255,3,1);
+  //set the viewport
+   glViewport(0,0,HEIGHT,WIDTH);
+  //set clear color
+   glClearColor(0.5f,0.5f,0.1f,1.0f);
   
 
    //loop
@@ -63,8 +74,9 @@ int main(int argc, char*argv[]){
          glClear(GL_COLOR_BUFFER_BIT);
         glfwPollEvents();
     }
-
+  
     glfwDestroyWindow(window);
+    glfwTerminate();
   
   return 0;
 }
